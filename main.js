abae0141ddf938842cde03817c6b9fc23384948a
Malik Gover <capybara2510@gmail.com>
2022-11-03 02:54:04 +0900

    Buttons and interface is working.

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", function(){
-    createBoard(32);
+    createBoard(16);
+    
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function(){
+        let size =getSize();
+        createBoard(size);
+    })
     
 })
 
@@ -12,7 +20,42 @@ function createBoard(size){
 
     for(let i = 0; i < numDivs; i++){
         let div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
+        div.addEventListener("mouseover", colorDiv)
         board.insertAdjacentElement("beforeend", div);
     }
+}
+
+
+function getSize(){
+    let input = prompt("Choose board size.");
+    let message = document.querySelector("#message");
+    if (input == ""){
+        message.innerHTML= "Please provide a value."
+    }
+    else if(input < 0 || input > 100){
+        message.innerHTML= "Please provide a value between 0 and 100."
+    }
+    else{
+        message.innerHTML="Now you can play!"
+        return input;
+    }
+}
+
+function colorDiv(){
+    if(color == "random"){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+    else{
+        this.style.backgroundColor = 'black'
+    }
+}
+
+function setColor(colorChoice){
+    color = colorChoice;
+
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll("div")
+    divs.forEach((div) => div.style.backgroundColor = "white")
 }
